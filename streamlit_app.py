a17a5e5d0db3b874e53eb4ed0e677781b459145e
rranjan010 <87009342+rranjan010@users.noreply.github.com>
2023-06-08 15:54:49 +0530

    Update streamlit_app.py
    pulling data into pandas data frame 
    displaying it on the page

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,6 @@ streamlit.text('Avocado toast')
 
 streamlit.header('Build your own smoothie')
 
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
